c1c5c26ec1166c0f8d1cc7cc7e0326d2b122e532
Przemyslaw Marcinkiewicz <przemek.marcinkiewicz@gmail.com>
2018-05-25 21:30:58 +0200

    Add configuration extensions to register data context.

---
+++ Marcinkiewicz.CqrsFramework.Data/Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Marcinkiewicz.CqrsFramework.Data.Configuration
+{
+    public static class ConfigurationExtensions
+    {
+        public static void AddDatabase(this IServiceCollection services, string connectionString)
+        {
+            // Add database
+            services.AddDbContext<DataContext>(
+                options => {
+                    options.UseSqlServer(connectionString);
+                });
+
+            // Register resolving by interface
+            services.AddScoped<IDataContext, DataContext>();
+        }
+    }
+}
